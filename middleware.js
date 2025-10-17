13d879c87b9d1a5c1f7fd068d2c257d53a5b2466
dhirajharane <dhirajharane@gmail.com>
2025-10-17 12:15:34 +0530

    fixing middleware

--- middleware.js
+++ middleware.js
@@ -1,24 +1,10 @@
-import { NextResponse } from "next/server";
-import { auth } from "@/lib/auth";
- // imp
-export const runtime = "nodejs";
+import { withAuth } from "next-auth/middleware";
 
-export async function middleware(req) {
-  const session = await auth();
-  const { pathname } = req.nextUrl;
-
-  const isPublicPath = pathname === '/login';
-
-  if (session && isPublicPath) {
-      return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
-
-  if (!session && !isPublicPath) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-  
-  return NextResponse.next();
-}
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
 
 export const config = {
   matcher: ["/dashboard/:path*"],
